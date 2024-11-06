2880aec0cc1fdc60d7191e4c72ecdd5ca47e3d6d
nihilKnight <takigawa_yoshino@163.com>
2024-11-06 13:59:32 +0800

    finished 20241106a.

--- src/main/java/com/example/myfirstspringboot/controller/HelloController.java
+++ src/main/java/com/example/myfirstspringboot/controller/HelloController.java
@@ -1,4 +1,12 @@
 package com.example.myfirstspringboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    @GetMapping("/hello")
+    public String Hello() {
+        return "hello, world!";
+    }
 }
